e923eddae2b187fe80b91962b4e293ff897c8702
myappsadda <humanashwani@gmail.com>
2026-02-09 00:29:10 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'cash-ledger-v1';
+const ASSETS = [
+  './',
+  './cash-ledger.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
